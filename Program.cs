cfdefdf65a4b8e1a5eedd271076088c016d141c8
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-30 18:46:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionMethods2.Extensions;
+
+namespace ExtensionMethods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
